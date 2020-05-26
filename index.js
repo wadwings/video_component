36eb6c8d53f27588ff256c0c115fed1027783cee
tongh <haidongtong@clubfactory.com>
2020-05-26 19:35:26 +0800

    bug-fixed

--- index.js
+++ index.js
@@ -291,7 +291,7 @@ eval("module.exports = function() {\r\n    let control = document.createElement(
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("let data = {\r\n    \"WitcherS01E06\":\"video/The.Witcher.S01E06.Rare.Species.1080p.NF.WEB-DL.DDP5.1.x264-NTG.mkv\",\r\n    \"WitcherS01E07\":\"video/The.Witcher.S01E07.Before.a.Fall.1080p.NF.WEB-DL.DDP5.1.x264-NTG.mkv\",\r\n    \"WitcherS01E08\":\"video/The.Witcher.S01E08.Much.More.1080p.NF.WEB-DL.DDP5.1.x264-NTG.mkv\",\r\n    \"小天大乱斗\":\"video/12.mp4\"\r\n}\r\n\r\nmodule.exports = exports = data;\n\n//# sourceURL=webpack:///./app/public/data.js?");
+eval("let data = {\r\n    \"WitcherS01E06\":\"/video/The.Witcher.S01E06.Rare.Species.1080p.NF.WEB-DL.DDP5.1.x264-NTG.mkv\",\r\n    \"WitcherS01E07\":\"/video/The.Witcher.S01E07.Before.a.Fall.1080p.NF.WEB-DL.DDP5.1.x264-NTG.mkv\",\r\n    \"WitcherS01E08\":\"/video/The.Witcher.S01E08.Much.More.1080p.NF.WEB-DL.DDP5.1.x264-NTG.mkv\",\r\n    \"小天大乱斗\":\"video/12.mp4\"\r\n}\r\n\r\nmodule.exports = exports = data;\n\n//# sourceURL=webpack:///./app/public/data.js?");
 
 /***/ }),
 
